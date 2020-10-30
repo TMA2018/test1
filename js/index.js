4e734a1c23f2146f4a763dcb157396c0b696f62e
Ivan <michma@mail.ru>
2020-10-30 21:56:23 +0200

    begin of project cinema

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 "use strict";
 
-console.log(1);
-let a = 5;
-const leftBordWidth = 5;
+let numberOfFilm = +prompt("сколько посмотрел фильмов?", 10);
 
-let pers = {
-    name: "John",
-    age: 25,
-    isMarried: true
+let persMovieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-
+let a = prompt("Name one of last films?","name_film");
+let b = +prompt("how is film?","10");
+let c = prompt("Name one of last films?","name_film");
+let d = +prompt("how is film?","10");
+persMovieDB.movies[a] = b;
+persMovieDB.movies[c] = d;
+console.log(persMovieDB);
+//2alert(persMovieDB.count);
 /*console.log(pers["name"]);
 //console.log(confirm("it's true?"));
 let b = +prompt("it's true", 6);
