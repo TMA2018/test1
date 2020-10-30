b78e9726bb51fbfc8d7c03fbc995eeb55e4dbce0
Ivan <michma@mail.ru>
2020-10-30 20:40:22 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+"use sctrict";
+
+console.log(1);
+let a = 5;
+const leftBordWidth = 5;
+
+let pers = {
+    name: "John",
+    age: 25,
+    isMarried: true
+};
+
+//console.log(pers["name"]);
+//console.log(confirm("it's true?"));
+let b = +prompt("it's true", 6);
+alert(typeof(b));
