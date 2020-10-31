7dbc01ef1c73666623b1a1b07ed87d656c1e1ce0
Ivan <michma@mail.ru>
2020-10-31 16:37:46 +0200

    change project in function

--- js/index.js
+++ js/index.js
@@ -1,39 +1,60 @@
-'use strict';
+"use strict";
 
-let numberOfFilm = +prompt("сколько посмотрел фильмов?", 10);
-
+let numberOfFilm ;
 let persMovieDB = {
-    count: numberOfFilm,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
-/*let a = prompt("Name one of last films?","name_film");
-let b = +prompt("how is film?","10");
-let c = prompt("Name one of last films?","name_film");
-let d = +prompt("how is film?","10");*/
-let a, b;
-while (true) {
-    
-    a = prompt("Name one of last films?","name_film");
-    if (a === null) {break;}
-    else if (a == '') {
-        alert ('name is empty, write real name'); 
-        continue; }
-    else if (a.length > 50) { 
-        alert ('name is long, write real name'); 
-        continue; 
+
+function start() {
+    numberOfFilm = +prompt("сколько посмотрел фильмов?", 10);
+    while ( numberOfFilm == '' || numberOfFilm == null || isNaN(numberOfFilm) ) {
+        numberOfFilm = +prompt("сколько посмотрел фильмов?", 10);
     }
-    b = +prompt("how is film?","10");
-    persMovieDB.movies[a] = b;
+    persMovieDB.count = numberOfFilm;
 }
 
-if (persMovieDB.count < 11){
-    console.log(`${persMovieDB.count} is small`);
-} else if (persMovieDB.count > 10 && persMovieDB.count<31){
-    console.log(`${persMovieDB.count} is norm`);
-} else {
-    console.log(`${persMovieDB.count} is cool`);
+function writeNameFilm () { 
+    let a, b;
+    while (true) {      
+        a = prompt("Name one of last films?","name_film");
+        if (a === null) {break;}
+        else if (a == '') {
+            alert ('name is empty, write real name'); 
+            continue; }
+        else if (a.length > 50) { 
+            alert ('name is long, write real name'); 
+            continue; 
+        }
+        b = +prompt("how is film?","10");
+        persMovieDB.movies[a] = b;
+    }
 }
-console.log(persMovieDB);
+
+function  detectUserFilmlevel () {
+    if (persMovieDB.count < 11){
+        console.log(`${persMovieDB.count} is small`);
+    } else if (persMovieDB.count > 10 && persMovieDB.count<31){
+        console.log(`${persMovieDB.count} is norm`);
+    } else {
+        console.log(`${persMovieDB.count} is cool`);
+    }
+}
+function showMyDB() {
+    if (!persMovieDB.privat) {console.log(persMovieDB);}
+}
+
+function writeYourGenres() {
+    for (let i=1; i < 4; i++) {
+        let a = prompt (`Write your lovest genres № ${i}`, 'comedy');
+        persMovieDB.genres[i-1] = a;
+    }
+}
+start();
+writeNameFilm();
+detectUserFilmlevel();
+writeYourGenres();
+showMyDB();
