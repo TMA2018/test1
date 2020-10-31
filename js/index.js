f3cdcabda8546c8f64a508914fcf108a7b090079
Ivan <michma@mail.ru>
2020-10-31 15:09:45 +0200

    cicles for cinema

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 let numberOfFilm = +prompt("сколько посмотрел фильмов?", 10);
 
@@ -9,15 +9,31 @@ let persMovieDB = {
     genres: [],
     privat: false
 };
-let a = prompt("Name one of last films?","name_film");
+/*let a = prompt("Name one of last films?","name_film");
 let b = +prompt("how is film?","10");
 let c = prompt("Name one of last films?","name_film");
-let d = +prompt("how is film?","10");
-persMovieDB.movies[a] = b;
-persMovieDB.movies[c] = d;
+let d = +prompt("how is film?","10");*/
+let a, b;
+while (true) {
+    
+    a = prompt("Name one of last films?","name_film");
+    if (a === null) {break;}
+    else if (a == '') {
+        alert ('name is empty, write real name'); 
+        continue; }
+    else if (a.length > 50) { 
+        alert ('name is long, write real name'); 
+        continue; 
+    }
+    b = +prompt("how is film?","10");
+    persMovieDB.movies[a] = b;
+}
+
+if (persMovieDB.count < 11){
+    console.log(`${persMovieDB.count} is small`);
+} else if (persMovieDB.count > 10 && persMovieDB.count<31){
+    console.log(`${persMovieDB.count} is norm`);
+} else {
+    console.log(`${persMovieDB.count} is cool`);
+}
 console.log(persMovieDB);
-//2alert(persMovieDB.count);
-/*console.log(pers["name"]);
-//console.log(confirm("it's true?"));
-let b = +prompt("it's true", 6);
-alert(typeof(b));*/
